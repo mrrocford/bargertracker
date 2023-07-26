d9c9e2e679b0507d03780c1b60531efb7c1a3b47
Олексий <oleksii@MacBook-home.local>
2023-07-27 02:21:32 +0300

    Change CryptoWeb

--- src/components/common/CryptoWeb.ts
+++ src/components/common/CryptoWeb.ts
@@ -1,8 +1,8 @@
-
-
 import Web3 from 'web3';
 
-const API_KEY = 'e6d6a8fe18b64e1d94f8bbb8ab6b91bb';
+const apiKey = process.env.REACT_APP_API_KEY; 
+
+const API_KEY = apiKey;
 
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://goerli.infura.io/v3/${API_KEY}`));
 
